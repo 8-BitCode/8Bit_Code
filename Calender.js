c089f9602e655eb85a159bc53ab65fc7afcfc183
8-BitCode <76620664+8-BitCode@users.noreply.github.com>
2021-05-29 16:13:03 +0100

    Delete Calender.js

--- Calender.js
+++
@@ -1,9 +0,0 @@
-function showSidebar() {
-    var x = document.getElementById('sidebar')
-    if (x.className == "sidebar") {
-        x.className += " show";
-    }
-    else {
-        x.className = "sidebar";
-    }
-}
